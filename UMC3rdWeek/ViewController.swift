5a410bde1268534a9ccb235e955ef3345844ab9d
Gi Wook Lee <108044639+chad0909@users.noreply.github.com>
2022-10-05 09:35:42 +0900

    Initial Commit

---
+++ UMC3rdWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC3rdWeek
+//
+//  Created by 077tech on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
